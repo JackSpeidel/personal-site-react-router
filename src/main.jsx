a69dac315eaec91886e089e3d7804ab5d08df47d
jack <speideljackson@protonmail.com>
2023-10-11 17:46:14 -0400

    deleted vite.svg; deleted App.jsx/scss; deleted react.svg; added index; changed styling on root & index; edited error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom"
+import './main.scss'
+import ErrorPage from "./error-page";
+import Root from "./routes/root/root";
+import Index from "./routes/index/index"
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                errorElement: <ErrorPage />,
+                children: [
+                    { index: true, element: <Index /> }
+                ]
+            }    
+        ]
+    }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={ router } />
   </React.StrictMode>,
 )
---
+++ src/routes/index/index.jsx
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const Index = () => {
+  return (
+    <div className="index">
+        <h1>I am a child element</h1>
+    </div>
+  )
+}
+
+export default Index
