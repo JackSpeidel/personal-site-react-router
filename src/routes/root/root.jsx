fa11fcebc64ceb3fa53b8530fb4ec20370ad00d8
jack <speideljackson@protonmail.com>
2023-10-26 12:21:31 -0400

    reformatting styling

--- src/routes/root/root.jsx
+++ src/routes/root/root.jsx
@@ -1,17 +1,17 @@
 import './root.scss';
-import { 
+import {
     Outlet,
- } from "react-router-dom";
- import Nav from "../../components/nav/nav";
- import Footer from "../../components/footer/footer";
+} from "react-router-dom";
+import Nav from "../../components/nav/nav";
+import Footer from "../../components/footer/footer";
 
 const Root = () => {
 
     return (
         <section className="app">
-                <Nav className="app__nav" />  
-                <Outlet className="app__outlet" />
-                <Footer className="app__footer" />
+            <Nav className="app__nav" />
+            <Outlet className="app__outlet" />
+            <Footer className="app__footer" />
         </section>
     )
 }
